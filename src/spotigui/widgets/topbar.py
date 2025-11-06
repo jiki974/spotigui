e1efedc446af3e0483a501c770bd5139e13b406e
jacky hoareau <jacky.hoareau@protonmail.com>
2025-11-06 07:36:25 +0100

    fix top bar background / font color

--- src/spotigui/widgets/topbar.py
+++ src/spotigui/widgets/topbar.py
@@ -16,10 +16,12 @@ class TopBarWidget(MDBoxLayout):
     """Top bar widget containing navigation, title, and device controls."""
 
     back_button_icon = StringProperty("arrow-left")
+    font_color = StringProperty("Custom")
 
     def __init__(
         self,
         back_button_icon: str = "arrow-left",
+        font_color: str =  "Custom",
         on_back: Optional[Callable] = None,
         on_device_select: Optional[Callable] = None,
         on_device_refresh: Optional[Callable] = None,
@@ -29,6 +31,7 @@ class TopBarWidget(MDBoxLayout):
         Initialize top bar widget.
 
         Args:
+            font_color: Color to be used for text and icons
             back_button_icon: Icon to show on the back button (default: "arrow-left")
             on_back: Callback when back button is pressed
             on_device_select: Callback when device is selected (device_id)
@@ -37,6 +40,7 @@ class TopBarWidget(MDBoxLayout):
         super().__init__(**kwargs)
 
         self.back_button_icon = back_button_icon
+        self.font_color = font_color
         self.on_back_callback = on_back
         self.on_device_select_callback = on_device_select
         self.on_device_refresh_callback = on_device_refresh
