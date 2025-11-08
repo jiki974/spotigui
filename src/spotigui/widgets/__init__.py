322e99afad264ad48a7dae7b935fe0bad85a3352
jacky hoareau <jacky.hoareau@protonmail.com>
2025-11-08 22:36:49 +0100

    remove dead code for progress bar

--- src/spotigui/widgets/__init__.py
+++ src/spotigui/widgets/__init__.py
@@ -1,8 +1,7 @@
 """Custom widgets for spotigui application."""
 
-from spotigui.widgets.progress_bar import ProgressBarWidget
 from spotigui.widgets.playlist_tile import PlaylistTile
 from spotigui.widgets.topbar import TopBarWidget
 
 
-__all__ = [ "ProgressBarWidget", "PlaylistTile", "TopBarWidget"]
+__all__ = ["PlaylistTile", "TopBarWidget"]
